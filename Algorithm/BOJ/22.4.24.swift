dd2ed70ff3429b5e80628ea0c1a7e7a04589283c
sanghunlee <simoniful0@gmail.com>
2022-04-24 15:08:39 +0900

    Add: BOJ 백트래킹

---
+++ Algorithm/BOJ/22.4.24.swift
@@ -0,0 +1,10 @@
+//
+//  22.4.24.swift
+//  Algorithm
+//
+//  Created by Sang hun Lee on 2022/04/24.
+//
+
+import Foundation
+
+
--- Algorithm/BOJ/main.swift
+++ Algorithm/BOJ/main.swift
@@ -5,44 +5,46 @@
 //  Created by Sang hun Lee on 2022/04/01.
 //
 
-// 2004번, 조합 0의 개수
-// 2, 5의 갯수를 비교
-// Combination == permutations / factorial == n! / (n - r)! * r!
-
 import Foundation
 
-let input = readLine()!.split(separator: " ").map{ Int($0)! }
+// 15649번, N과 M (1)
+// 백 트래킹으로 다시 풀이 필요
 
-let N = input[0]
-var M = input[1]
+let inputValue = readLine()!.split(separator: " ").map{ Int($0)! }
+let N = inputValue[0]
+let M = inputValue[1]
 
-func get2(_ num: Int) -> Int {
-    var count = 0
-    var i = 2
-    while num >= i {
-        count += num/i
-        i *= 2
+let arrN = [Int](1...N)
+
+func permute(_ nums: [Int], _ targetNum: Int) -> [[Int]] {
+    var result = [[Int]]()
+    var visited = [Bool](repeating: false, count: nums.count)
+    
+    func permutation(_ nowPermute: [Int]) {
+        if nowPermute.count == targetNum {
+            result.append(nowPermute)
+            return
+        }
+        for i in 0..<nums.count {
+            if visited[i] == true {
+                continue
+            }
+            else {
+                visited[i] = true
+                permutation(nowPermute + [nums[i]])
+                visited[i] = false
+            }
+        }
     }
-    return count
+    permutation([])
+    
+    return result
 }
 
-func get5(_ num: Int) -> Int {
-    var count = 0
-    var i = 5
-    while num >= i {
-        count += num/i
-        i *= 5
-    }
-    return count
-}
+let result = permute(arrN, M).map{ $0.map{ String($0) }}
 
-let a = get2(N) - (get2(M) + get2(N - M))
-let b = get5(N) - (get5(M) + get5(N - M))
-
-print(min(a, b))
-
-
-
+Array(Set(result)).forEach{ print($0.joined(separator: " ")) }
+ 
 
 
 
