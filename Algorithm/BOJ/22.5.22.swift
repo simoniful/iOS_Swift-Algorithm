8cbe24173b83cc9d90a6b0fbf2239e02d37b76f6
sanghunlee <simoniful0@gmail.com>
2022-05-23 13:08:49 +0900

    Add: BOJ 큐, 덱 (2) & README.md 구성

--- Algorithm/BOJ/22.5.22.swift
+++ Algorithm/BOJ/22.5.22.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+
+// 1021번, 회전하는 큐
+
+let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
+let n = firstLine[0]
+let m = firstLine[1]
+var targets = readLine()!.split(separator: " ").map { Int($0)! }
+var deque = [Int](1...n)
+var answer = 0
+
+enum Direction {
+    case right
+    case left
+}
+
+func rotate(_ direction: Direction) {
+    switch direction {
+    case .right:
+        let temp = deque.removeLast()
+        deque.insert(temp, at: 0)
+    case .left:
+        let temp = deque.removeFirst()
+        deque.append(temp)
+    }
+}
+
+func find() {
+    let target = targets[0]
+    guard let targetIndex = deque.firstIndex(of: target) else { return }
+    if target == deque[0] {
+        targets.removeFirst()
+        deque.removeFirst()
+    } else if targetIndex > deque.count / 2 {
+        rotate(.right)
+        answer += 1
+    } else {
+        rotate(.left)
+        answer += 1
+    }
+}
+
+while !targets.isEmpty {
+    find()
+}
+
+print(answer)
+
+// 5430번, AC
+// 시간초과
+
+let testCase = Int(readLine()!)!
+
+for _ in 0..<testCase {
+    let commands = Array(readLine()!).map{ String($0) }
+    let arrCount = Int(readLine()!)!
+    var arr = readLine()!
+        .replacingOccurrences(of: "[\\[\\]]", with: "", options: .regularExpression)
+        .split(separator: ",").map { Int(String($0))! }
+
+    for command in commands {
+        if command == "R" {
+            arr.reverse()
+        } else if command == "D" {
+            if !arr.isEmpty {
+                arr.removeFirst()
+            }
+        }
+    }
+
+    if arr.isEmpty {
+        print("error")
+    } else {
+        let arrStr = arr.map{ String($0) }.joined(separator: ",")
+        print("[\(arrStr)]")
+    }
+}
+
+
--- Algorithm/BOJ/22.5.4.swift
+++ Algorithm/BOJ/22.5.4.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+
+// 1966번, 프린터 큐
+
+let testCase = Int(readLine()!)!
+
+for _ in 1...testCase {
+    let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
+    let m = firstLine[1]
+    
+    var printCount = 0
+    
+    var secondLine = readLine()!.split(separator: " ").map{ Int($0)! }
+    var result: [(Int, Int)] = []
+    
+    for (index, priority) in secondLine.enumerated() {
+        let tuple = (priority, index)
+        result.append(tuple)
+    }
+    
+    secondLine.sort()
+    
+    while true {
+        if result[0].0 == secondLine.max() {
+            printCount += 1
+            if result[0].1 == m {
+                print(printCount)
+                break
+            } else {
+                result.removeFirst()
+                secondLine.popLast()
+            }
+        } else {
+            result.append(result.removeFirst())
+        }
+    }
+}
+
+// 10866번, 덱
+
+var deque:[Int] = []
+
+for _ in 0..<Int(readLine()!)! {
+    let C = readLine()!.split{$0==" "}.map{String($0)}
+    let command = C.first!
+    var num = 0
+    
+    if C.count > 1 {
+        num = Int(C.last!)!
+    }
+    
+    switch command {
+    case "push_front":
+        deque.insert(num, at: 0)
+    case "push_back":
+        deque.append(num)
+    case "pop_front":
+        print(deque.isEmpty ? -1 : deque.removeFirst())
+    case "pop_back":
+        print(deque.isEmpty ? -1 : deque.removeLast())
+    case "size":
+        print(deque.count)
+    case "empty":
+        print(deque.isEmpty ? 1:0)
+    case "front":
+        print(deque.isEmpty ? -1 : deque.first!)
+    default:
+        print(deque.isEmpty ? -1 : deque.last!)
+    }
+}
