26701377fbd2e310e3b8d1ced4332119e9e813b4
sanghunlee <simoniful0@gmail.com>
2022-10-10 22:41:55 +0900

    Add: Queue 테스트 정리

--- Algorithm/Raywenderlich/Queue/Tests/QueueTestCase.swift
+++ Algorithm/Raywenderlich/Queue/Tests/QueueTestCase.swift
@@ -31,24 +31,40 @@ import XCTest
 @testable import DataStructures
 
 final class QueueTestCase: XCTestCase {
+  var queueArray = QueueArray<String>()
+  var queueStack = QueueStack<String>()
   
   override func setUp() {
-
+    queueArray.enqueue("Pusheen")
+    queueArray.enqueue("Keyboard Cat")
+    queueArray.enqueue("Lil Bub")
+    
+    queueStack.enqueue("Milo")
+    queueStack.enqueue("Sassy")
+    queueStack.enqueue("Ozma")
   }
   
   func test_enqueueArray() {
-    
+    XCTAssertEqual(queueArray.peek, "Pusheen")
   }
   
   func test_enqueueStack() {
-    
+    XCTAssertEqual(queueStack.peek, "Milo")
   }
   
   func test_dequeueArray() {
-
+    queueArray.dequeue()
+    queueArray.dequeue()
+    XCTAssertEqual(queueArray.peek, "Lil Bub")
+    queueArray.dequeue()
+    XCTAssertTrue(queueArray.isEmpty)
   }
   
   func test_dequeueStack() {
-
+    queueStack.dequeue()
+    queueStack.dequeue()
+    XCTAssertEqual(queueStack.peek, "Ozma")
+    queueStack.dequeue()
+    XCTAssertTrue(queueStack.isEmpty)
   }
 }
