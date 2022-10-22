28a2ea77dde9b27537ab03e6b5bc9d66ac50b532
sanghunlee <simoniful0@gmail.com>
2022-10-22 13:33:08 +0900

    Add: Binary Search 이론 및 테스트 정리

--- Algorithm/Raywenderlich/Binary Search/DataStructures/Tests/BinarySearchTestCase.swift
+++ Algorithm/Raywenderlich/Binary Search/DataStructures/Tests/BinarySearchTestCase.swift
@@ -32,6 +32,6 @@ import XCTest
 final class BinarySearchTestCase: XCTestCase {
   func test_binarySearch() {
     let array = [1, 5, 18, 32, 33, 33, 47, 49, 502]
-
+    XCTAssertEqual(array.binarySearch(for: 5), 1)
   }
 }
