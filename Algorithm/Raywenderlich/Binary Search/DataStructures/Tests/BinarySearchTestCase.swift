1946e1934f8fff6272d79055f067a8e4d9bd54a3
sanghunlee <simoniful0@gmail.com>
2022-11-01 23:26:26 +0900

    Add: Binary Search Challenge 정리

--- Algorithm/Raywenderlich/Binary Search/DataStructures/Tests/BinarySearchTestCase.swift
+++ Algorithm/Raywenderlich/Binary Search/DataStructures/Tests/BinarySearchTestCase.swift
@@ -30,6 +30,11 @@ import XCTest
 @testable import DataStructures
 
 final class BinarySearchTestCase: XCTestCase {
+  func test_findIndices() {
+    let array = [1, 2, 3, 3, 3, 4, 5, 5]
+    XCTAssertEqual(findIndices(of: 3, in: array), 2..<5)
+  }
+  
   func test_binarySearch() {
     let array = [1, 5, 18, 32, 33, 33, 47, 49, 502]
     XCTAssertEqual(array.binarySearch(for: 5), 1)
